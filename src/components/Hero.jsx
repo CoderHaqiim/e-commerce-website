4bbb2fd67b9094610ad2e46f687462c7522984bb
Haqiim <hqcoder.jh@gmail.com>
2024-03-27 12:16:06 +0100

    swiper speed changed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 const Hero = () => {
     return (
         <div className='mb-10'>
-            <swiper-container slides-per-view="1" duration="3000" speed="50" loop="true" navigation="false" pagination="true" autoplay="true"> 
+            <swiper-container slides-per-view="1" speed="1000npm " loop="true" navigation="false" pagination="true" autoplay="true"> 
                 <swiper-slide className="w-full">
                     <div className='w-full h-[70vh] bg-yellow-200 py-[2%] '>
                         <div className="w-full bg-baseColor mx-auto h-full flex justify-center items-center text-white">slide1</div>
