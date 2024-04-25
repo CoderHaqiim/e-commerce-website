e00f4850f8f8ceb667c74df8a9ddfd6be4b4085c
Haqiim <hqcoder.jh@gmail.com>
2024-04-25 12:22:28 +0100

    Some more adjustment

--- src/components/Discount.jsx
+++ src/components/Discount.jsx
@@ -4,20 +4,20 @@ import Card from './Card';
 
 const Discount = ({title,display}) => {
     return (
-        <div className='w-full  h-[auto] bg-white m-auto'>
-            <div className='container w-[70%] sm:w-[90%] md:w-[80%] h-auto mx-auto'>
+        <div className='w-full h-[auto] bg-white m-auto flex items-center justify-center'>
+            <div className='container w-full md:w-[80%] px-[20px]'>
                 <div>
-                    <Titlehead title={title}/>
-                    <div className='text-baseColor3 text-[1rem] text-grey md:text-[1.2rem]'>
-                        <strong>Get more with less with our discount offers</strong>
-                    </div>
+                        <Titlehead title={title}/>
+                        <div className='text-baseColor3 text-[1rem] text-grey md:text-[1.2rem]'>
+                            <strong>Get more with less with our discount offers</strong>
+                        </div>
                 </div>
-                <div className=' discount flex w-[100%] h-[200px] items-center overflow-x-scroll pl-[10%] px-[10px] my-[30px] gap-[20px] md:jusitify-center'>
+                <div className=' discount flex w-[100%] h-[200px] items-center overflow-x-scroll px-[10px] my-[30px] gap-[20px] md:jusitify-center'>
                     {display.map((item,index)=>item.discount?<Card key={item.id} item= {item} type="discount"/>:<></>)}
                 </div>
             </div>
         </div>
-    );
+    )
 }
 
 export default Discount;
--- src/components/Showcase.jsx
+++ src/components/Showcase.jsx
@@ -7,16 +7,16 @@ import { Link } from 'react-router-dom';
 const Showcase = ({display,filteritems,title,itemId,all,useItemId}) => {
 
             return (
-                <div className='w-full h-auto overflow-y-hidden my-10 m-auto rounded-md'>
-                    <div className='container w-[70%] h-auto mx-auto sm:w-[90%] md:w-[80%]'>
-                        <div>
-                            <Titlehead title={title}/>
-                            <div className='text-baseColor3 text-[1rem] text-grey md:text-[1.2rem] '>
-                                <strong>Browse each categories for items thoughfully selected for you</strong>
-                            </div>
+                <div className='w-full h-auto overflow-y-hidden my-10 m-auto rounded-md flex justify-center items-center'>
+                    <div className='container w-full md:w-[80%] px-[20px]'>
+                        <div className='w-full'>
+                                <Titlehead title={title}/>
+                                <div className='text-baseColor3 text-[1rem] text-grey md:text-[1.2rem] '>
+                                    <strong>Browse each categories for items thoughfully selected for you</strong>
+                                </div>
                         </div>
             
-                        <div className="container w-full h-full flex gap-[1.5rem] justify-center py-[2%] my-[30px] flex-wrap mx-auto">
+                        <div className=" w-full h-full flex gap-[1.5rem] justify-center py-[2%] my-[30px] flex-wrap mx-auto">
                             {
                                all?
                                 display.map((item)=><div key={item.id}><Card key={item.id} filteritems={filteritems} type='home' id = {item.type} item={item} useItemId={useItemId} itemId={itemId}/></div>):
